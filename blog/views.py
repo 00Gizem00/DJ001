23acb12aded36cf5a9f847f6d39f85ce2604147c
Gizem Yılmaz <64495695+00Gizem00@users.noreply.github.com>
2023-09-07 02:07:00 +0300

    index duzenledim

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, News
 
 def index(request):
+    # Fetch the "Music" category
+    music_category = get_object_or_404(Category, slug='music')
+
+    # Query the last 6 news items in the "Music" category
+    music_news = News.objects.filter(category=music_category).order_by('-created')[:6]
+    tech_news = News.objects.filter(category__slug='tech').order_by('-created')[:5]
+    game_news = News.objects.filter(category__slug='game').order_by('-created')[:5]
+    magazine_news = News.objects.filter(category__slug='magazine').order_by('-created')[:5]
+    anime_news = News.objects.filter(category__slug='anime').order_by('-created')[:5]
+
     # Fetch all categories
     categories = Category.objects.all()
 
@@ -14,7 +24,7 @@ def index(request):
         if latest_news:
             latest_news_by_category.append(latest_news)
 
-    return render(request, 'pages/index.html', {'latest_news_by_category': latest_news_by_category, 'categories': categories},)
+    return render(request, 'pages/index.html', {'latest_news_by_category': latest_news_by_category, 'categories': categories, 'music_news': music_news, 'tech_news': tech_news, 'game_news': game_news, 'magazine_news': magazine_news, 'anime_new': anime_news},)
 
 def category_detail(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
