acd673d700a21f41fd0810998ff936c8a976c622
Gizem Yılmaz <64495695+00Gizem00@users.noreply.github.com>
2023-09-07 00:30:32 +0300

    navbar duzeltildi. her kategoriden son haberler alindi vsvs

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,27 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, News
 
 def index(request):
+    # Fetch all categories
     categories = Category.objects.all()
-    return render(request, 'pages/index.html', {'categories': categories})
+
+    # Create a list to store the latest news for each category
+    latest_news_by_category = []
+
+    # Query the latest news for each category
+    for category in categories:
+        latest_news = News.objects.filter(category=category).order_by('-created').first()
+        if latest_news:
+            latest_news_by_category.append(latest_news)
+
+    return render(request, 'pages/index.html', {'latest_news_by_category': latest_news_by_category, 'categories': categories},)
 
 def category_detail(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
     news_in_category = News.objects.filter(category=category).order_by('-created')
-    return render(request, 'pages/category_detail.html', {'category': category, 'news_in_category': news_in_category})
+    categories = Category.objects.all()  # Pass the categories to the context
+    return render(request, 'pages/category_detail.html', {'category': category, 'news_in_category': news_in_category, 'categories': categories})
 
 def news_detail(request, news_slug):
     news = get_object_or_404(News, slug=news_slug)
-    return render(request, 'pages/news_detail.html', {'news': news})
+    categories = Category.objects.all()  # Pass the categories to the context
+    return render(request, 'pages/news_detail.html', {'news': news, 'categories': categories})
